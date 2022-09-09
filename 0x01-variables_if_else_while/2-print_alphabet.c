aff08aee84b9b0c7781b8a67374f49d4d5a0fdc7
Fanyafulu <fanyafulu@yahoo.com>
2022-09-09 16:30:11 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,14 @@
 
 /**
 * main - print alphabet in lowercase
-* you can only use the putchar 
-* Return: 0
+* you can only use the putchar
+* Return: Always 0.
 */
-
 int main(void)
 {
 int l = 'a';
 while (l <= 'z')
-{	
+{
 putchar(l);
 l += 1;
 }
