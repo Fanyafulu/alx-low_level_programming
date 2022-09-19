5cf6fb8ac87f37454eef55242beff50814551952
Fanyafulu <fanyafulu@yahoo.com>
2022-09-19 08:49:15 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts - pusts to stdout
+* @str: params
+*/
+
+void _puts(char *str)
+{
+int i = 0;
+while (str[i] != '\0');
+{
+-putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
