3c2131115399ea864d90dd718a6c3888aba8edf5
Fanyafulu <fanyafulu@yahoo.com>
2022-09-23 13:49:27 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,6 +12,7 @@
 void print_line(char *c, int s, int l)
 {
 int j, k;
+
 for (j = 0; j <= 9; j++)
 {
 if (j <= s)
@@ -36,10 +37,10 @@ putchar('.')
 * @size: size of buffer
 * Return: void
 */
-
 void print_buffer(char *b, int size)
 {
 int i;
+
 for (i = 0; i <= (size - 1) / 10 && size; i++)
 {
 printf("%08x: ", i * 10);
