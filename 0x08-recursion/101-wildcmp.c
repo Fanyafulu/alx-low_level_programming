fd6bdd45b4d90b322c80b592fb9254d9d8291d13
Fanyafulu <fanyafulu@yahoo.com>
2022-09-28 10:37:54 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,7 +3,8 @@
 /**
 * wildcmp - Compare strings
 * @s1: pointer to string params
-* @s2: pointer to string params* Return: 0
+* @s2: pointer to string params
+* Return: 0
 */
 
 int wildcmp(char *s1, char *s2)
