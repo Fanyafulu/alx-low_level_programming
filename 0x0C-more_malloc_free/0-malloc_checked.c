09ed0e618a9d4d213b3886f900618de7bfc311dd
Uwabu <uwabudifrank1@gmail.com>
2022-10-06 13:05:38 +0100

    '0-malloc_checked.c'

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,9 @@
-#include "main.h"
+#include "stdlib.h"
 
 /**
-*malloc_checked - array for prints a string
-*@b: number of memory
-*Return: void
+*malloc_checked - allocates memory using malloc.
+*@b: size of memory to be allocated.
+*Return: pointer to allocated memory.
 */
 
 void *malloc_checked(unsigned int b)
@@ -15,6 +15,6 @@ p = malloc(b);
 if (p == NULL)
 {
 exit(98);
-}
+
 return (p);
 }
