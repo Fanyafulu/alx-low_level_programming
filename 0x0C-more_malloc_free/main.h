b2fb0cfadf8dfda98543e2cf10b387d9089b413f
Uwabu <uwabudifrank1@gmail.com>
2022-10-06 13:18:01 +0100

    '0-malloc_checked.c'

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _puts(char c);
+int _puts(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmedb, unsigned int size);
@@ -9,6 +9,6 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int _atoi(const char *s);
-void print _int(unsigned long int n);
+void print_int(unsigned long int n);
 
 #endif
