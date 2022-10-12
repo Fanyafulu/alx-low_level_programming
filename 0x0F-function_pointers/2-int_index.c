34784b0afd70155fee05bc5cb5e2ac54c086e466
Fanyafulu <fanyafulu@yahoo.com>
2022-10-12 07:00:59 -0700

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,8 +13,10 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int index;
+
 if (array == NULL || cmp == NULL)
 return (-1);
+
 for (index = 0; index < size; index++)
 {
 if (cmp(array[index]) != 0)
