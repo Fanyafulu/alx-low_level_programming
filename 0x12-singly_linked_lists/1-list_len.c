edccebe17742472da3a6d0ed91fbaa14f134f337
Fanyafulu <fanyafulu@yahoo.com>
2022-10-21 05:51:47 -0700

    list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *
+ * list_len - Return the number of elements in
+ *
+ * a list.
+ *
+ * @h: stringly linked list.
+ *
+ * Return: The number of elements in the list.
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+size_t nbr_nodes = 0;
+while (h != NULL)
+{
+nbr_nodes++;
+h = h->next;
+}
+return (nbr_nodes);
+}
