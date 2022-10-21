9d1d752a69cccd3420ee63b44a2d2f15511713dc
Fanyafulu <fanyafulu@yahoo.com>
2022-10-21 08:41:52 -0700

    advance task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,13 +3,13 @@
 void __attribute__((constructor)) hare(void);
 
 /**
-* bmain function executed before main
+* hare - Prints a string before the
+* bmain function executed.
 * Return: no return.
 */
 
 void hare(void)
 
 {
-printf("You're beat! and yet, you must allow")
-printf(",\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n" "I bore my house upon my back!\n");
 }
