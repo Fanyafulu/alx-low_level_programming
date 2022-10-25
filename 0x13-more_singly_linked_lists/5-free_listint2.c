f93dc44d42ad23deff503282ee985a1d5a5b9d06
Fanyafulu <fanyafulu@yahoo.com>
2022-10-24 20:38:28 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,23 +2,20 @@
 
 /*
 * free_listp2 - frees a linked list
-* @head: head of a list.
-* Return: no return.
+* @head: pointer a list.
+* Return: void.
 */
 
 void free_listin2(listin_t **head)
 {
 listin_t *temp;
-listin_t *curr;
 
 if (head != NULL)
 {
-curr = *head;
-while ((temp = curr) != NULL)
-{
-curr = curr->next;
+while (*head)
+temp = *head;
+*head = (*head)->next;
 free(temp);
 }
-*head = NULL;
-}
+head = NULL;
 }
