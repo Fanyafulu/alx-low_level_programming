5ee8f851574f249fce190c37d0c607f464b1b9b8
Fanyafulu <fanyafulu@yahoo.com>
2022-11-04 05:30:14 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,6 +43,7 @@ exit(100);
 }
 
 }
+
 /**
 *main - Copies the contents of a file to another file.
 *@argc: The number of arguments supplied to the program.
@@ -53,10 +54,12 @@ exit(100);
 *If file_to cannot be created or written to - exit code 99.
 *If file_to or file_from cannot be closed - exit code 100.
 */
+
 int main(int argc, char *argv[])
 {
 int from, to, r, w;
 char *buffer;
+
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -66,7 +69,8 @@ buffer = create_buffer(argv[2]);
 from = open(argv[1], O_RDONLY);
 r = read(from, buffer, 1024);
 to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-do {
+do 
+{
 if (from == -1 || r == -1)
 {
 dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
@@ -74,6 +78,7 @@ free(buffer);
 exit(98);
 }
 w = write(to, buffer, r);
+
 if (to == -1 || w == -1)
 {
 dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
@@ -82,7 +87,8 @@ exit(99);
 }
 r = read(from, buffer, 1024);
 to = open(argv[2], O_WRONLY | O_APPEND);
-} while (r > 0);
+}
+while (r > 0);
 free(buffer);
 close_file(from);
 close_file(to);
