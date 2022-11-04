6ea5bb8bdbbd91f69cd08156278b1b4f5eeee453
Fanyafulu <fanyafulu@yahoo.com>
2022-11-04 05:17:40 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -220,6 +220,7 @@ if (e_ident[EI_DATA] == ELFDATA2MSB)
 e_entry = ((e_entry << 8) & 0xFF00FF00) |((e_entry >> 8) & 0xFF00FF);
 e_entry = (e_entry << 16) | (e_entry >> 16);
 }
+
 if (e_ident[EI_CLASS] == ELFCLASS32)
 printf("%#x\n", (unsigned int)e_entry);
 else
